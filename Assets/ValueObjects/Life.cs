55240431c4e0b3556e9521f5453ac70f5b968330
Parrish Sanchez <ppaarriisshh@gmailcom>
2022-01-10 15:23:14 -0600

    Feat: Add Life.cs

---
+++ Assets/ValueObjects/Life.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace ValueObjects
+{
+    [Serializable]
+    public class Life
+    {
+        public static float MaximumAllowed = 1000;
+        [SerializeField] private float value;
+        public float Value => value;
+        
+        public Life(float amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Life amount must be positive : " + amount);
+            }
+
+            if (amount > MaximumAllowed)
+            {
+                throw new ArgumentException("Life have a maximum of " + MaximumAllowed + ", get " + amount);
+            }
+            value = amount;
+        }
+
+        public Life Damage(Life amount)
+        {
+            var result = Value - amount.Value;
+            return new Life(result < 0 ? 0 : result );
+        }
+        
+        public Life Heal(Life amount)
+        {
+            var result = Value + amount.value;
+            return new Life(result > MaximumAllowed ? MaximumAllowed : result);
+        }
+    }
+}
